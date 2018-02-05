72a8fcd9f68a27e53a24d4ad8dcfc8205e5e9cbe
Renato Amaral <srkimbo@gmail.com>
2018-02-05 14:49:51 +0000

    Adding isOdd function exercise

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num) {
+  return num % 2 === 1;
+}
+
+//var tenIsEven = isEven(10);
+//var elevenIsEven = isEven(11);
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
